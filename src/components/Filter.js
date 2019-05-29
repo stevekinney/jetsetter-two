c3d5642b156fb2b9bc7f5d722f5c88329f11a0d2
Steve Kinney <hello@stevekinney.net>
2019-05-28 19:41:32 -0500

    Refactor Filter to hooks

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React, { useCallback } from 'react';
 
-class Filter extends Component {
-  handleChange = event => {
-    const { onChange } = this.props;
-    const value = event.target.value;
-    onChange(value);
-  };
+const Filter = ({ searchTerm, onChange }) => {
+  const handleChange = useCallback(
+    event => {
+      const value = event.target.value;
+      onChange(value);
+    },
+    [onChange],
+  );
 
-  render() {
-    const { searchTerm } = this.props;
-    return (
-      <input
-        className="Items-searchTerm"
-        value={searchTerm}
-        onChange={this.handleChange}
-      />
-    );
-  }
-}
+  return (
+    <input
+      className="Items-searchTerm"
+      value={searchTerm}
+      onChange={handleChange}
+    />
+  );
+};
 
 export default Filter;
